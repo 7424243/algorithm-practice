53e6673dde9ef2e4c2584dd0e125f833d659983a
Sam Sheets <sam.sheets94@gmail.com>
2021-05-14 16:48:53 -0600

    complete aVeryBigSum function

--- arrays.js
+++ arrays.js
@@ -159,3 +159,15 @@ function compareTriplets(a, b) {
     results.push(bobPoints)
     return results
 }//O(n)
+
+/* ----- A Very Big Sum ----- */
+/*In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.*/
+//Input: [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
+//Output: 5000000015
+function aVeryBigSum(ar) {
+    let sum = 0
+    for(let i = 0; i < ar.length; i++) {
+        sum+=ar[i]
+    }
+    return sum
+}//O(n)
